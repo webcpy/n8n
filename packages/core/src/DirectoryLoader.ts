abe062148a9b09b24dffec84cb65b7a64b37c630
webcpy <webcpy@163.com>
2024-04-30 18:03:50 +0800

    feat

--- packages/core/src/DirectoryLoader.ts
+++ packages/core/src/DirectoryLoader.ts
@@ -288,7 +288,7 @@ export abstract class DirectoryLoader {
 		if (obj.icon?.startsWith('file:')) {
 			const iconPath = path.join(path.dirname(filePath), obj.icon.substring(5));
 			const relativePath = path.relative(this.directory, iconPath);
-			obj.iconUrl = `icons/${this.packageName}/${relativePath}`;
+			obj.iconUrl = `icons/${this.packageName}/${relativePath}`.replace('nodes_zh', 'nodes');
 			delete obj.icon;
 		}
 	}
